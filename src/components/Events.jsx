faf590e3cf45058966436fc07fe774fdfb016f6e
azizbg1 <aziz.benguirat@esprit.tn>
2024-02-29 11:34:59 +0100

    form

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,12 +1,12 @@
-import Event from "./Event"
-import listEvent from "../data/events.json"
 import {Row} from "react-bootstrap"
 import { useEffect, useState } from "react"
 import Alert from 'react-bootstrap/Alert';
+import { getallEvents } from "../service/api";
 
 export default function Events(){
     const [showAlert,setshowAlert]= useState(false)
     const [showWelcome,setshowWelcome]=useState(false)
+    const[listEvent,setListEvents ]=useState([])
     const modifAlert=()=>{
         setshowAlert(true);
         setTimeout(()=>setshowAlert(false),1000);
@@ -18,6 +18,14 @@ export default function Events(){
        console.log("unmounting")
        
     }},[])
+    useEffect(()=>{
+        const fetshlist=async()=>{
+        const events=await getallEvents();
+        setListEvents(events.data);
+        console.log(events); 
+        }
+        fetshlist();
+    })
     return <>
     {showWelcome && <Alert variant="success">
         bienvenue
